2a1add1a99983972b5795e80e9b74f223820df7d
Angel <22824872+Zyplos@users.noreply.github.com>
2021-06-07 16:43:06 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
 import { Link } from "@theme-ui/components";
 import { Link as RouterLink } from "react-router-dom";
 
-
-export default class Header extends Component {
-    render() {
-        return(
-          <div className="header">
-              <Link className="logo" as={RouterLink} to="/profile">
-                Market Monitor
-              </Link>
-              <div className="sub-header">
-                <Link as={RouterLink} to="/login">
-                    Log in
-                </Link>
-              </div>
-          </div>
-        )
-      }
+function Header() {
+  return (
+    <div className="header">
+      <Link className="logo" as={RouterLink} to="/profile">
+        Market Monitor
+      </Link>
+      <div className="sub-header">
+        <Link as={RouterLink} to="/login">
+          Log in
+        </Link>
+      </div>
+    </div>
+  );
 }
+
+export default Header;
